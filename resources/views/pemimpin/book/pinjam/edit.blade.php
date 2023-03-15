c1062011e4ea843816601716eeb394fa322d3875
Afwan <tmariefafwan@gmail.com>
2023-03-16 00:32:14 +0700

    komit kosong

--- resources/views/pemimpin/book/pinjam/edit.blade.php
+++ resources/views/pemimpin/book/pinjam/edit.blade.php
@@ -5,7 +5,7 @@
 <div class="container-fluid">
     <h1 class="h3 mb-2 text-gray-800">{{ $page }}</h1>
 
-        <form action="{{ route('pengarang.update',$pengarang->id) }}" method="POST" enctype="multipart/form-data">
+        <form action="{{ route('pinjam.update',$pinjam->id) }}" method="POST" enctype="multipart/form-data">
             @csrf
             @method('PUT')
 
@@ -13,13 +13,13 @@
                 <div class="box box-primary">
                     <div class="box-body">
                         <div class="form-group">
-                            <label for="prodi">Nama Pengarang</label>
+                            <label for="prodi">Nama Member</label>
                             <input type="text" name="name" class="form-control" id="name" value="{{ $pengarang->name }}" required>
                         </div>
                     </div>
                 </div>
                 <div class="box-footer">
-                    <a href="{{ route('pengarang.index') }}" class="btn btn-danger btn-flat">
+                    <a href="{{ route('pinjam.index') }}" class="btn btn-danger btn-flat">
                         Kembali
                     </a>
                     <button type="submit" class="btn btn-success btn-flat">Edit</button>
